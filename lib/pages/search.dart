6418d9783b1c0e516d235966158114b70f0752b4
mazzardi <ljungmanjimmy@hotmail.com>
2020-04-30 13:37:59 +0200

    - Added Search-page UI - Added basic functionality to Search-page

--- lib/pages/search.dart
+++ lib/pages/search.dart
@@ -1,9 +1,52 @@
 import 'package:flutter/material.dart';
 
+//TODO: kladdig klass, ska städa lite
 class Search extends StatelessWidget{
+
   @override
   Widget build(BuildContext context) {
-    // TODO: implement build
-    throw UnimplementedError();
+    textFieldController.addListener(_printLatestValue);
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Colors.grey[850],
+        centerTitle: true,
+        title: Text('Search'),
+      ),
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.start,
+        children: <Widget>[
+          Container(
+            padding: const EdgeInsets.all(20.0),
+            child: TextFormField(
+              decoration: InputDecoration(
+                hintText: "Search"
+              ),
+              keyboardType: TextInputType.text,
+              controller: textFieldController,
+            ),
+          )
+        ],
+      )
+    );
+  }
+
+
+  //TODO
+  /**
+   * Borde denna metod egentligen ligga i main?
+   *
+   *  @override
+      void dispose(){
+      textFieldController.dispose();
+      print("testing, disposed.");
+      }
+   *
+   * https://flutter.dev/docs/cookbook/forms/retrieve-input
+   */
+  final textFieldController = TextEditingController();
+
+  _printLatestValue(){
+    print("testing: ${textFieldController.text}");
   }
 }
+
