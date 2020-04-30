b9e82d7f31c5e9ddc7c924d2a7edb39c1135cb0a
mazzardi <ljungmanjimmy@hotmail.com>
2020-04-30 11:03:42 +0200

    - Updated dependencies for Google Maps and HTTP - Updated Map class to Dannes version

--- lib/pages/mapPage.dart
+++ lib/pages/mapPage.dart
@@ -1,9 +1,71 @@
 import 'package:flutter/cupertino.dart';
+import 'dart:async';
+import 'dart:convert';
 
-class MapPage extends StatelessWidget{
+import 'package:google_maps_flutter/google_maps_flutter.dart';
+import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
+
+class MapPage extends StatelessWidget {
+  Set<Marker> markers = Set();
+
+  getData() async {
+    markers = await getDogParkMarkers();
+
+  }
+
+  GoogleMapController mapController;
+
+  void _onMapCreated(GoogleMapController controller) {
+    mapController = controller;
+    //todo: tog bort en line kod här som inte passade. Måste fråga danne.
+  }
+
   @override
   Widget build(BuildContext context) {
-    // TODO: implement build
-    throw UnimplementedError();
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text('Hundparker'),
+          backgroundColor: Colors.green[700],
+        ),
+        body: GoogleMap(
+          markers: Set.from(
+            markers,
+          ),
+          onMapCreated: _onMapCreated,
+          initialCameraPosition: CameraPosition(
+            target: LatLng(59.2948966, 17.913921),
+            zoom: 11.0,
+          ),
+        ),
+      ),
+    );
   }
+
+  Future<Set<Marker>> getDogParkMarkers() async {
+    final response =
+    await http.get('https://github.com/danne332/test/raw/master/hund.json'); //todo: ändra http request till databasen istället
+
+    if (response.statusCode == 200) {
+      Set<Marker> tempMarkers = new Set<Marker>();
+      List listOfParks = json.decode(response.body);
+      for (int i = 0; i < listOfParks.length; i++) {
+        tempMarkers.add(Marker(
+          //icon:
+          //  BitmapDescriptor.defaultMarkerWithHue(BitmapDescriptor.hueGreen),
+            markerId: MarkerId(i.toString()),
+            infoWindow: InfoWindow(
+              title: listOfParks[i]['name'],
+              snippet: listOfParks[i]['desc'],
+            ),
+            position: LatLng(listOfParks[i]['lat'], listOfParks[i]['long'])));
+      }
+      return tempMarkers;
+    } else {
+      throw Exception('Failed to fetch hund.json');
+    }
+  }
+
+
 }
