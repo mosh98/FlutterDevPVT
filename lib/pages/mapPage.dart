da8cdc28da8050c3993f81c70a538b45d3083601
danne332 <vestin.daniel1@gmail.com>
2020-05-01 17:12:32 +0200

    Senaste mapPage

--- lib/pages/mapPage.dart
+++ lib/pages/mapPage.dart
@@ -1,71 +1,281 @@
-import 'package:flutter/cupertino.dart';
 import 'dart:async';
 import 'dart:convert';
 
-import 'package:google_maps_flutter/google_maps_flutter.dart';
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:google_maps_flutter/google_maps_flutter.dart';
+import 'package:location/location.dart';
 import 'package:http/http.dart' as http;
 
+
 class MapPage extends StatelessWidget {
-  Set<Marker> markers = Set();
-
-  getData() async {
-    markers = await getDogParkMarkers();
-
-  }
-
-  GoogleMapController mapController;
-
-  void _onMapCreated(GoogleMapController controller) {
-    mapController = controller;
-    //todo: tog bort en line kod här som inte passade. Måste fråga danne.
-  }
-
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: Scaffold(
-        appBar: AppBar(
-          title: Text('Hundparker'),
-          backgroundColor: Colors.green[700],
-        ),
-        body: GoogleMap(
-          markers: Set.from(
-            markers,
-          ),
-          onMapCreated: _onMapCreated,
-          initialCameraPosition: CameraPosition(
-            target: LatLng(59.2948966, 17.913921),
-            zoom: 11.0,
-          ),
-        ),
+      title: 'Hundappen',
+      home: TheMapScreen(),
+    );
+  }
+}
+
+class TheMapScreen extends StatefulWidget {
+  TheMapScreen({Key key, this.title}) : super(key: key);
+
+  final String title;
+
+  @override
+  _TheMapScreenState createState() => _TheMapScreenState();
+}
+
+class _TheMapScreenState extends State<TheMapScreen> {
+  final Location location = Location();
+  Completer<GoogleMapController> _controller = Completer();
+
+  final int _MAX_PARK_DISTANCE = 1000;
+  final int _MAX_TRASH_BIN_DISTANCE = 1000;
+
+
+  Set<Marker> _markers = Set();
+
+  Set<Marker> _wasteBinMarkers = Set();
+  Set<Marker> _dogParksMarkers = Set();
+  Marker _searchPosMarker = null;
+  Marker _homePosMarker = null;
+
+  // Har den nuvarande positionen av mobilen
+  LocationData _currentLocation = null;
+
+  // Har positionen man markerat på kartan
+  LatLng _searchLocation = null;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Maps'),
+        centerTitle: true,
+      ),
+      body: Stack(
+        children: <Widget>[
+          googleMapsWidget(),
+          Container(
+              alignment: Alignment.topRight,
+              padding: EdgeInsets.all(20),
+              child: Column(
+                children: <Widget>[
+                  findParksButton(),
+                  SizedBox(height: 10.0),
+                  findTrashBinsButton(),
+                  SizedBox(height: 10.0),
+                  homeButton()
+                ],
+              )),
+        ],
       ),
     );
   }
 
-  Future<Set<Marker>> getDogParkMarkers() async {
-    final response =
-    await http.get('https://github.com/danne332/test/raw/master/hund.json'); //todo: ändra http request till databasen istället
+  Widget googleMapsWidget() {
+    return GoogleMap(
+        mapType: MapType.normal,
+        markers: _markers,
+        initialCameraPosition: CameraPosition(
+          target: LatLng(59.339843, 18.045731),
+          zoom: 10.0,
+        ),
+        onMapCreated: (GoogleMapController controller) {
+          _controller.complete(controller);
+        },
+        onTap: _onMapTap);
+  }
 
-    if (response.statusCode == 200) {
-      Set<Marker> tempMarkers = new Set<Marker>();
-      List listOfParks = json.decode(response.body);
-      for (int i = 0; i < listOfParks.length; i++) {
-        tempMarkers.add(Marker(
-          //icon:
-          //  BitmapDescriptor.defaultMarkerWithHue(BitmapDescriptor.hueGreen),
-            markerId: MarkerId(i.toString()),
-            infoWindow: InfoWindow(
-              title: listOfParks[i]['name'],
-              snippet: listOfParks[i]['desc'],
-            ),
-            position: LatLng(listOfParks[i]['lat'], listOfParks[i]['long'])));
+  Widget homeButton() {
+    return Material(
+      shape: CircleBorder(),
+      color: Colors.cyanAccent,
+      child: IconButton(
+          icon: Icon(Icons.home),
+          iconSize: 50,
+          tooltip: 'Hem',
+          onPressed: () {
+            _homeButtonPressed();
+          }),
+    );
+  }
+
+  void _onMapTap(LatLng point) {
+    if (point != null) {
+      if (_searchPosMarker != null) {
+        _markers.remove(_searchPosMarker);
       }
-      return tempMarkers;
-    } else {
-      throw Exception('Failed to fetch hund.json');
+      _searchLocation = LatLng(point.latitude, point.longitude);
+      _searchPosMarker = new Marker(
+          markerId: MarkerId('SEARCH MARKER'),
+          icon:
+          BitmapDescriptor.defaultMarkerWithHue(BitmapDescriptor.hueBlue),
+          position:
+          LatLng(_searchLocation.latitude, _searchLocation.longitude));
+
+      setState(() {
+        _markers.add(_searchPosMarker);
+      });
+
+
+
     }
   }
 
+  void _homeButtonPressed() {
+    _setMyCurrentLocation();
+    if (_currentLocation != null) {
 
+
+      if (_homePosMarker != null) {
+        _markers.remove(_homePosMarker);
+      }
+      // För att uppdatera kartan och se nya markerna
+      _homePosMarker = Marker(
+          markerId: MarkerId('HOME MARKER'),
+          icon: BitmapDescriptor.defaultMarkerWithHue(
+              BitmapDescriptor.hueCyan),
+          position: LatLng(
+              _currentLocation.latitude, _currentLocation.longitude));
+
+
+      setState(() {
+        _markers.add(_homePosMarker);
+      });
+
+    }
+
+    _gotoLocation(_currentLocation);
+  }
+
+
+  Future<void> _setMyCurrentLocation() async {
+    final LocationData _locationResult = await location.getLocation();
+    _currentLocation = _locationResult;
+  }
+
+  Future<void> _gotoLocation(LocationData location) async {
+    final GoogleMapController controller = await _controller.future;
+    controller.animateCamera(CameraUpdate.newCameraPosition(CameraPosition(
+        target: LatLng(location.latitude, location.longitude), zoom: 15.0)));
+  }
+
+  Future<String> _getJSON(String url) async {
+    final response = await http.get(url);
+    return response.body;
+  }
+
+  Future<void> _locateParks() async {
+    if (_searchLocation != null) {
+      _dogParksMarkers.clear();
+
+
+      String url = "https://pvt-dogpark.herokuapp.com/dogpark/find?latitude=";
+      url += _searchLocation.latitude.toString() + "&longitude=";
+      url += _searchLocation.longitude.toString() + "&distance=";
+      url += _MAX_PARK_DISTANCE.toString();
+      String data = await _getJSON(url);
+
+      List dataAsList = jsonDecode(data);
+
+
+      for (int i = 0; i < dataAsList.length; i++) {
+        _dogParksMarkers.add(Marker(
+            markerId: MarkerId('DOG_PARK_' + i.toString()),
+
+            icon: BitmapDescriptor.defaultMarkerWithHue(
+                BitmapDescriptor.hueGreen),
+            infoWindow: InfoWindow(title: dataAsList[i]['name'], snippet: dataAsList[i]['description']),
+
+            position:
+            LatLng(dataAsList[i]['latitude'], dataAsList[i]['longitude'])));
+      }
+
+      setState(() {
+
+
+        _markers.clear();
+        _markers.addAll(_dogParksMarkers);
+
+        if (_searchPosMarker != null)
+          _markers.add(_searchPosMarker);
+        if (_homePosMarker != null)
+          _markers.add(_homePosMarker);
+        if (!_wasteBinMarkers.isEmpty) {
+          _markers.addAll(_wasteBinMarkers);
+        }
+
+      });
+    }
+  }
+
+  Future<void> _locateTrashBins() async {
+    if (_searchLocation != null) {
+      String url = "https://pvt-dogpark.herokuapp.com/wastebin/find?latitude=";
+      url += _searchLocation.latitude.toString() + "&longitude=";
+      url += _searchLocation.longitude.toString() + "&distance=";
+      url += _MAX_TRASH_BIN_DISTANCE.toString();
+      String data = await _getJSON(url);
+
+      List dataAsList = jsonDecode(data);
+
+      _wasteBinMarkers.clear();
+
+
+      for (int i = 0; i < dataAsList.length; i++) {
+        _wasteBinMarkers.add(Marker(
+            markerId: MarkerId('TRASH_BIN_' + i.toString()),
+            infoWindow: InfoWindow(title: 'Papperskorg'),
+
+            icon: BitmapDescriptor.defaultMarkerWithHue(
+                BitmapDescriptor.hueMagenta),
+            position:
+            LatLng(dataAsList[i]['latitude'], dataAsList[i]['longitude'])));
+
+      }
+
+      setState(() {
+        _markers.clear();
+        _markers.addAll(_wasteBinMarkers);
+
+        if (_searchPosMarker != null)
+          _markers.add(_searchPosMarker);
+        if (_homePosMarker != null)
+          _markers.add(_homePosMarker);
+        if (!_dogParksMarkers.isEmpty) {
+          _markers.addAll(_dogParksMarkers);
+        }      });
+    }
+
+  }
+
+
+  Widget findParksButton() {
+    return Material(
+      shape: CircleBorder(),
+      color: Colors.green,
+      child: IconButton(
+        icon: Icon(Icons.collections),
+        iconSize: 50,
+        tooltip: 'Hitta parker',
+        onPressed: _locateParks,
+      ),
+    );
+  }
+
+  Widget findTrashBinsButton() {
+    return Material(
+      shape: CircleBorder(),
+      color: Colors.lightBlue,
+      child: IconButton(
+        icon: Icon(Icons.delete),
+        iconSize: 50,
+        tooltip: 'Hitta papperskorgar',
+        onPressed: _locateTrashBins,
+      ),
+    );
+  }
 }
