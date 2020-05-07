1b187f9e7cbdee4267da007cdb7dc58330170c4a
mazzardi <ljungmanjimmy@hotmail.com>
2020-05-07 18:34:56 +0200

    - Refined startPage code - Refined UI design - Random facts added to StartPage - Loader added to LoginPage, not finished.

--- lib/pages/StartPage.dart
+++ lib/pages/StartPage.dart
@@ -6,67 +6,113 @@ import 'mapPage.dart';
 import 'package:http/http.dart' as http;
 
 class StartPage extends StatelessWidget {
-
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-        appBar: AppBar(
-          backgroundColor: Colors.grey[850],
-        ),
-        body: Center(
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: <Widget>[
-                MaterialButton(
-                  color: Colors.teal,
-                  textColor: Colors.white,
-                  height: 50.0,
-                  minWidth: 150.0,
-                  child: new Text("login"),
-                  onPressed: () {
-                    Navigator.of(context).push(MaterialPageRoute<Null>(
-                        builder: (BuildContext context) {
-                          return new LoginPage();
-                        }));
-                  },
-                ),
-                Padding(child: Text("OR"), padding: EdgeInsets.all(10.0)),
-                MaterialButton(
-                  color: Colors.teal,
-                  textColor: Colors.white,
-                  height: 50.0,
-                  minWidth: 150.0,
-                  child: new Text("View map"),
-                  onPressed: () {
-                    Navigator.of(context).push(
-                        MaterialPageRoute(builder: (BuildContext context) {
-                          return new MapPage();
-                        }));
-                  },
-                ),
-              ],
-            )
+    return MaterialApp(
+        home: StatefulStartPage(),
+        theme: ThemeData(
+          primarySwatch: Colors.blue,
         )
     );
   }
 }
-  
-//  String _getFunFact(){ //TODO: FINISH METHOD
-//    final response = http.get('https://some-random-api.ml/facts/dog');
-//
-//    if (response != null) {
-//      Map f = jsonDecode(response.toString());
-//    }
-//    return "";
-//  }
-//}
-//
-//class RandomFact{
-//  final String fact;
-//
-//  RandomFact(this.fact);
-//
-//  RandomFact.fromJson(Map<String,dynamic> json): fact = json['fact'];
-//
-//  Map<String,dynamic> toJson() => {'fact' : fact};
-//}//https://flutter.dev/docs/development/data-and-backend/json
+
+class StatefulStartPage extends StatefulWidget{
+  @override
+  State createState() => new StartPageState();
+}
+
+class StartPageState extends State<StatefulStartPage>{
+
+  String fact = "";
+
+  @override
+  void initState() {
+    super.initState();
+    _getFunFact();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        body: ListView(
+          padding: const EdgeInsets.fromLTRB(10, 250, 10, 0),
+          children: <Widget>[
+            Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: <Widget>[
+                _factSection(),
+                _buttonSection(),
+              ],
+            )
+          ],
+        )
+    );
+  }
+
+  Widget _factSection(){
+    return Container(
+      child: Column(
+        children: <Widget>[
+          Text(
+            fact.toLowerCase(),
+            overflow: TextOverflow.visible,
+            style: TextStyle(
+              fontSize: 20,
+              fontStyle: FontStyle.italic,
+            ),
+          ),
+          SizedBox(height:10),
+          Text('Anyways..'),
+        ],
+      ),
+    );
+  }
+
+  Widget _buttonSection(){
+    return Container(
+        padding: EdgeInsets.only(top:15),
+        child: Column(
+          children: <Widget>[
+            MaterialButton(
+              height: 40.0,
+              minWidth: 250.0,
+              color: Colors.black54,
+              textColor: Colors.white,
+              child: new Text("Login"),
+              onPressed: () {
+                Navigator.of(context).push(MaterialPageRoute<Null>(
+                    builder: (BuildContext context) {
+                      return new LoginPage();
+                    }));
+              },
+              shape: new RoundedRectangleBorder(borderRadius: new BorderRadius.circular(30.0)),
+            ),
+            Text('OR'),
+            MaterialButton(
+                height: 40.0,
+                minWidth: 250.0,
+                color: Colors.black54,
+                textColor: Colors.white,
+                child: new Text("View map"),
+                onPressed: () {
+                  Navigator.of(context).push(
+                      MaterialPageRoute(builder: (BuildContext context) {
+                        return new MapPage();
+                      }));
+                },
+                shape: new RoundedRectangleBorder(borderRadius: new BorderRadius.circular(30.0))
+            ),
+          ],
+        )
+    );
+  }
+
+  void _getFunFact() async{
+    final response = await http.get('https://some-random-api.ml/facts/dog');
+    if(response.statusCode == 200){
+      Map<String,dynamic> f = json.decode(response.body);
+      fact = f['fact'];
+    }
+  }
+}
