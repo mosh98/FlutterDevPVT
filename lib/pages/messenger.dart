058d67a9aafa232c6bd01891ff2e228289c66d84
moslehmahamud <mosleh.edu@gmail.com>
2020-05-31 14:02:23 +0200

    updated notification, not done yet

--- lib/pages/messenger.dart
+++ lib/pages/messenger.dart
@@ -1,14 +1,61 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
+import 'package:firebase_messaging/firebase_messaging.dart';
 import 'package:flutter/cupertino.dart';
+import 'package:http/http.dart' as http;
+
+import 'dart:async';
+import 'dart:convert';
 
 class Messenger extends StatelessWidget {
+
   final databaseReference = Firestore.instance;
   final String recipient = "norp@florp.com";
   FirebaseAuth auth = FirebaseAuth.instance;
   FirebaseUser user;
 
+  final FirebaseMessaging _firebaseMessaging = FirebaseMessaging();
+
+  Future<Map<String, dynamic>> sendAndRetrieveMessage(String body, String title) async {
+    await _firebaseMessaging.requestNotificationPermissions(
+      const IosNotificationSettings(
+          sound: true, badge: true, alert: true, provisional: false),
+    );
+
+    String teken = 'dSlNvWn9-FQ:APA91bHU3vCNpLz6tHMW8GSFJzqGDl_2B2j7uoDYeMSjMg_ac9lmdtDCKIFiElTUZDezNUvBCHm0wOA4nf-23ADkbTUvmJJvN02eRBCMMec9DMqhXH8K9qrJJff609c9Rnu6GNOP3XMe';
+
+    await http.post(
+      'https://fcm.googleapis.com/fcm/send',
+      headers: <String, String>{
+        'Content-Type': 'application/json',
+        'Authorization': 'key=AAAApxhRlHQ:APA91bHl1eBjWN0jTAwguFZKAWPES8DnTa5A7Akw-DSrQiG4mE2lDo-12kzWLke1Kj1rAZ00yguG9FOsLZCODNHLq1-wZOLa_Ny1hKBz-7pRt3mgc8F4FgYk5nykcX7yBstZIQ4-8uuk',
+        //authrization is the firebase CloudStore server key
+      },
+
+      body: jsonEncode(
+        <String, dynamic>{
+          'notification': <String, dynamic>{
+            'body': '$body',
+            'title': '$title'
+          },
+          'priority': 'high',
+          'data': <String, dynamic>{
+            'click_action': 'FLUTTER_NOTIFICATION_CLICK',
+            'id': '1',
+            'status': 'done'
+          },
+          'to': teken,
+        },
+      ),
+    );
+  }
+
   void _onSendMessage(String content, String uid, String peerUid) {
+
+    String senderToken;
+    _firebaseMessaging.getToken().then((value) => senderToken = value);
+
+    //This user
     Firestore.instance
         .collection('users')
         .document(user.email)
@@ -19,8 +66,11 @@ class Messenger extends StatelessWidget {
         .setData({
       'from': recipient,
       'text': content,
-      'timestamp': DateTime.now().toIso8601String().toString()
+      'timestamp': DateTime.now().toIso8601String().toString(),
+      'senderToken': senderToken
     });
+
+    //The other user or the recipient
     Firestore.instance
         .collection('users')
         .document(recipient)
@@ -43,6 +93,7 @@ class Messenger extends StatelessWidget {
 }
 
 class Message extends StatelessWidget {
+
   final String message;
   final String timeStamp;
   final String nameUser;
@@ -75,3 +126,9 @@ class Message extends StatelessWidget {
     );
   }
 }
+
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    throw UnimplementedError();
+  }
